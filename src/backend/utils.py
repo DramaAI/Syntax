58a436ee5b83a5a08cf70db02dbf8ff713f138ab
Luca Vivona <lucavivona01@gmail.com>
2023-11-25 17:03:55 -0500

    add preprocessing function for model fitting

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -1,11 +1,13 @@
 import torch
 import os
+import re
+import pandas as pd
 
 FILEPATH = os.path.dirname(os.path.realpath(__file__))
 LOCAL_BASE_MODEL = os.path.join(FILEPATH, "data", 'weights', 'bert-base-uncased.pt')
 LOCAL_TOKENIZER  = os.path.join(FILEPATH, "data", 'tokenizer', 'tokenizer.pt')
 
-__all__ = ["load_base_model", "load_tokenizer"]
+__all__ = ["load_base_model", "load_tokenizer", "process_data"]
 
 def load_base_model():
     model = None 
@@ -28,3 +30,58 @@ def load_tokenizer():
         token = torch.load(LOCAL_TOKENIZER)
     return token
 
+
+
+def ASSERTION_DATAFRAME_COLUMN(columns):
+    columns = tuple(columns)
+    DATASET_COLUMNS_WITH_ID = ("ID", "Input", "Output")
+    DATASET_COLUMNS = ("Input", "Output")
+    return DATASET_COLUMNS == columns or DATASET_COLUMNS_WITH_ID == columns
+
+
+def preprocessed(df, tokenizer, vocab=30522, context_length=512, flag="~_~"):
+    """"""
+    
+    assert isinstance(df, pd.DataFrame), "pre-process precondition needs to be in a DataFrame"
+    assert ASSERTION_DATAFRAME_COLUMN(df.columns)
+
+    # parse string to convert into a list
+    df["Output"] = df["Output"].apply(lambda x : x.split(','))
+
+    # regular expression that obtains flag and associated word
+    pattern = rf"{flag}(\w+)"
+
+    # if ID exist within the column then remove it
+    if "ID" in df.columns:
+        df.drop("ID", axis=1, inplace=True)
+
+    inputs, outputs = [], []
+    for i, sentence in enumerate(df["Input"]):
+        replace = re.findall(pattern, sentence) # find all replaced tokens
+        replacement : list = df["Output"][i]    # index the associated replacement list
+        
+        # replace tokens should equal replacement
+        if len(replacement) != len(replace):
+            continue
+
+        # construct the replacement dictionary
+        replacement_dict = {replace : replacement[i] for i, replace in enumerate(replace)}   
+
+        # TODO Not ideal in initializing each iteration 
+        def replaced(match):
+            word = match.group(1)
+            return replacement_dict.get(word, match.group(0))
+
+        output_text = re.sub(pattern, replaced, sentence)
+        input_text = sentence.replace(flag, "")
+        inputs.append(input_text)
+        outputs.append(output_text)
+
+
+    input_tokens = tokenizer(inputs, padding=True, max_length=context_length, truncation=True, return_tensors="pt")
+    # TODO: change to make 
+    output_tokens = tokenizer(outputs, padding=True, max_length=context_length, truncation=True, return_tensors="pt")
+    mask = ((input_tokens["input_ids"] - output_tokens["input_ids"]) != 0).to(torch.long)
+    prob = torch.nn.functional.one_hot(output_tokens["input_ids"], num_classes=vocab)
+    
+    return input_tokens, output_tokens, mask, prob
